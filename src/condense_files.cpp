779abcfe5b98d587e72d3ee86d7dde79483d8e36
Simon Walker <s.r.walker101@googlemail.com>
2014-08-17 15:50:41 +0100

    Getting number of files

--- src/condense_files.cpp
+++ src/condense_files.cpp
@@ -1,9 +1,33 @@
 #include "condense_files.h"
 
+#include <fstream>
+#include <iostream>
+
 using namespace std;
 
-FileCondenser::FileCondenser(const string &filelist) {
+namespace {
+    long get_filelist_length(const string &filelist) {
+        long counter = 0;
+        ifstream infile(filelist);
+        if (infile.is_open()) {
+            while (!infile.eof()) {
+                counter++;
+            }
+        }
+        infile.close();
+        return counter;
+    }
+}
+
+FileCondenser::FileCondenser(const string &filelist)
+: _filelist(filelist) {
+    _nfiles = get_filelist_length(_filelist);
+    cout << "Working with " << _nfiles << "files" << endl;
 }
 
 void FileCondenser::render(const string &output) {
+    load_data();
+}
+
+void FileCondenser::load_data() {
 }

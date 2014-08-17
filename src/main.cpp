f4fe74dc8996bea6ca83d0c0bc2c4d626c16877e
Simon Walker <s.r.walker101@googlemail.com>
2014-08-17 15:42:18 +0100

    Adds outer imperative code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
-#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <fitsio.h>
+#include <tclap/CmdLine.h>
 
-int main() {
-    return 0;
+#include "condense_files.h"
+
+int main(int argc, const char *argv[]) {
+    try {
+        TCLAP::CmdLine cmd("Condense", ' ', "0.0.1");
+
+        TCLAP::UnlabeledValueArg<std::string> filelist_arg("filelist", "List of files to condense", true, "", "filelist", cmd);
+        TCLAP::ValueArg<std::string> output_arg("o", "output", "Resulting output file", true, "", "fits file", cmd);
+
+        cmd.parse(argc, argv);
+
+        return 0;
+
+    } catch (TCLAP::ArgException &e) {
+        std::cerr << "Argument error: " << e.error() << " for arg " << e.argId() << std::endl;
+        return 1;
+    }
 }

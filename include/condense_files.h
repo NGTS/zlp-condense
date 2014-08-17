abadc3bdbe51c6e2c71fc196d6b3cca0d8b64e4b
Simon Walker <s.r.walker101@googlemail.com>
2014-08-17 21:04:22 +0100

    Adding catalogue and imagelist objects

--- include/condense_files.h
+++ include/condense_files.h
@@ -4,8 +4,27 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <fitsio.h>
 
 #include "filelist.h"
+
+struct Catalogue {
+    std::vector<std::string> obj_id;
+    std::vector<double> mean_flux;
+    std::vector<long> npts;
+
+    Catalogue(long n) :
+        obj_id(std::vector<std::string>(n)),
+        mean_flux(std::vector<double>(n)),
+        npts(std::vector<long>(n)) {}
+};
+
+struct Imagelist {
+    std::vector<double> airmass;
+
+    Imagelist(long n) :
+        airmass(std::vector<double>(n)) {}
+};
 
 class FileCondenser {
     public:
@@ -18,6 +37,8 @@ class FileCondenser {
         std::unique_ptr<FileList> _filelist;
 
         std::vector<double> _mjd_arr, _flux_arr, _skybkg_arr, _ccdx_arr, _ccdy_arr;
+        std::unique_ptr<Catalogue> _catalogue;
+        std::unique_ptr<Imagelist> _imagelist;
 
         void load_data();
         void read_file(const std::string &fname, long index);

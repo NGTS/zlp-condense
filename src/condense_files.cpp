7ed7727733dbbff4fd77dc2228bac7c1ab0de225
Simon Walker <s.r.walker101@googlemail.com>
2014-08-17 18:22:43 +0100

    Adds ccdx, ccdy and renders all to disk

--- src/condense_files.cpp
+++ src/condense_files.cpp
@@ -7,6 +7,12 @@
 #include "filelist.h"
 #include "fits_column.h"
 
+#define RENDER_ARRAY(data, name) \
+    fits_create_img(fptr, DOUBLE_IMG, 2, naxes, &status); \
+    fits_write_pix(fptr, TDOUBLE, fpixel, _nfiles * _napertures, &data[0], &status); \
+    fits_write_key_str(fptr, "EXTNAME", name, NULL, &status);
+
+
 using namespace std;
 
 FileCondenser::FileCondenser(const string &filelist_name)
@@ -22,6 +28,28 @@ void FileCondenser::render(const string &output) {
         index++;
     }
 
+    fitsfile *fptr;
+    int status = 0;
+    long naxes[] = {_nfiles, _napertures};
+    long fpixel[] = {1, 1};
+
+    fits_create_file(&fptr, output.c_str(), &status);
+
+    // Make a blank first image
+
+    // Now the other hdus
+    RENDER_ARRAY(_mjd_arr, "HJD");
+    RENDER_ARRAY(_flux_arr, "FLUX");
+    RENDER_ARRAY(_skybkg_arr, "SKYBKG");
+    RENDER_ARRAY(_ccdx_arr, "CCDX");
+    RENDER_ARRAY(_ccdy_arr, "CCDY");
+
+    fits_close_file(fptr, &status);
+
+    if (status) {
+        fits_report_error(stderr, status);
+        exit(status);
+    }
 
 }
 
@@ -42,13 +70,15 @@ void FileCondenser::initialize() {
     _mjd_arr.resize(_nfiles * _napertures);
     _flux_arr.resize(_nfiles * _napertures);
     _skybkg_arr.resize(_nfiles * _napertures);
+    _ccdx_arr.resize(_nfiles * _napertures);
+    _ccdy_arr.resize(_nfiles * _napertures);
 }
 
 void FileCondenser::read_file(const string &fname, long index) {
     fitsfile *fptr;
     int status = 0;
     double mjd = 0;
-    FitsColumn flux(_napertures), skybkg(_napertures);
+    FitsColumn flux(_napertures), skybkg(_napertures), ccdx(_napertures), ccdy(_napertures);
 
     fits_open_file(&fptr, fname.c_str(), READONLY, &status);
     fits_movrel_hdu(fptr, 1, 0, &status);
@@ -57,6 +87,8 @@ void FileCondenser::read_file(const string &fname, long index) {
 
     flux.read(fptr, "core2_flux");
     skybkg.read(fptr, "skylev");
+    ccdx.read(fptr, "x_coordinate");
+    ccdy.read(fptr, "y_coordinate");
 
     for (long ap=0; ap<_napertures; ap++) {
         long _write_index = index * _napertures + ap;
@@ -64,6 +96,8 @@ void FileCondenser::read_file(const string &fname, long index) {
         _mjd_arr[_write_index] = mjd;
         flux.write(&_flux_arr[_write_index], ap);
         skybkg.write(&_skybkg_arr[_write_index], ap);
+        ccdx.write(&_ccdx_arr[_write_index], ap);
+        ccdy.write(&_ccdy_arr[_write_index], ap);
     }
 
     fits_close_file(fptr, &status);

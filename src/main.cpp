579c9fc9db3a50d54c8163291aab8473cd2e2412
Simon Walker <s.r.walker101@googlemail.com>
2015-05-10 18:04:23 +0100

    Start condense files function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+#include <tclap/CmdLine.h>
 #include <fitsio.h>
 #include <fits++.h>
-#include <tclap/CmdLine.h>
+
+void condense_files(const std::vector<std::string> &files,
+        const std::string &output_filename) {
+}
 
 int main(int argc, char *argv[]) {
 
@@ -10,9 +17,11 @@ int main(int argc, char *argv[]) {
         TCLAP::CmdLine cmd("ZLP condense", ' ', "0.1");
         TCLAP::ValueArg<std::string> output_arg(
             "o", "output", "Output file name", true, "", "file", cmd);
-        TCLAP::UnlabeledMultiArg<std::string> multi(
+        TCLAP::UnlabeledMultiArg<std::string> input_files_arg(
             "files", "Files to condense", true, "file", cmd);
         cmd.parse(argc, argv);
+
+        condense_files(input_files_arg.getValue(), output_arg.getValue());
 
         return 0;
 

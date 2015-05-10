3f0f20fd1f61a3ba3ba1203abd7cdfc87d7c4392
Simon Walker <s.r.walker101@googlemail.com>
2015-05-10 18:02:19 +0100

    Add initial command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <fitsio.h>
 #include <fits++.h>
+#include <tclap/CmdLine.h>
 
-int main() {
-    return 0;
+int main(int argc, char *argv[]) {
+
+    try {
+
+        TCLAP::CmdLine cmd("ZLP condense", ' ', "0.1");
+        TCLAP::ValueArg<std::string> output_arg(
+            "o", "output", "Output file name", true, "", "file", cmd);
+        TCLAP::UnlabeledMultiArg<std::string> multi(
+            "files", "Files to condense", true, "file", cmd);
+        cmd.parse(argc, argv);
+
+        return 0;
+
+    } catch (TCLAP::ArgException &e) {
+        std::cerr << "error: " << e.error() << " for arg " << e.argId()
+                  << std::endl;
+        return 1;
+    }
 }

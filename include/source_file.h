4e0d143fda6e729afc7f16ad1173bdfe1c6e2f4c
Simon Walker <s.r.walker101@googlemail.com>
2015-05-12 16:49:22 +0100

    Run clang-format
    Former-commit-id: fd5857247e10470819b6b3d4ceedc6f48c2ba636

--- include/source_file.h
+++ include/source_file.h
@@ -7,25 +7,25 @@
 #include "fits++.h"
 
 class SourceFile {
-  public:
-    SourceFile(const std::string &filename);
-    ~SourceFile();
+public:
+  SourceFile(const std::string &filename);
+  ~SourceFile();
 
-    void addData(const std::string &source_key, fitspp::FITSImage *imageHDU,
-                 int image, int napertures);
-    void addMJD(fitspp::FITSImage *imageHDU, int image, int napertures);
-    fitspp::FITSKeyword readKeyword(const std::string &name) const;
-    vector<double> readData(const std::string &source_key, int napertures) const;
-    vector<double> readData(const std::string &source_key) const;
+  void addData(const std::string &source_key, fitspp::FITSImage *imageHDU,
+               int image, int napertures);
+  void addMJD(fitspp::FITSImage *imageHDU, int image, int napertures);
+  fitspp::FITSKeyword readKeyword(const std::string &name) const;
+  vector<double> readData(const std::string &source_key, int napertures) const;
+  vector<double> readData(const std::string &source_key) const;
 
-  private:
-    void writeData(vector<double> &data, fitspp::FITSImage *imageHDU, int image,
-                   int napertures);
+private:
+  void writeData(vector<double> &data, fitspp::FITSImage *imageHDU, int image,
+                 int napertures);
 
-    fitspp::FITSFile *f;
-    fitspp::FITSBinaryTable *table;
-    std::string filename;
-    double nullval;
+  fitspp::FITSFile *f;
+  fitspp::FITSBinaryTable *table;
+  std::string filename;
+  double nullval;
 };
 
 #endif /* end of include guard: SOURCE_FILE_H */
--- include/table.h
+++ include/table.h
@@ -11,39 +11,38 @@
 #include "source_file.h"
 
 class Table {
-  public:
-    Table(const std::string &tablename);
+public:
+  Table(const std::string &tablename);
 
-    void initialise(fitspp::FITSFile *f,
-                    const std::string &filename, long size);
+  void initialise(fitspp::FITSFile *f, const std::string &filename, long size);
 
-    void add(const std::string &name, double value);
-    void add(const std::string &name, long value);
-    void add(const std::string &name, int value);
-    void render();
-    void addFromSourceFile(const SourceFile &s);
+  void add(const std::string &name, double value);
+  void add(const std::string &name, long value);
+  void add(const std::string &name, int value);
+  void render();
+  void addFromSourceFile(const SourceFile &s);
 
-  protected:
-    std::set<std::string> colNames;
-    fitspp::FITSBinaryTable *table;
+protected:
+  std::set<std::string> colNames;
+  fitspp::FITSBinaryTable *table;
 
-  private:
-    std::string tablename;
-    std::map<std::string, std::string> typeMap;
-    std::map<std::string, std::vector<double>> doubleMap;
-    std::map<std::string, std::vector<int>> intMap;
-    std::map<std::string, std::vector<long>> longMap;
+private:
+  std::string tablename;
+  std::map<std::string, std::string> typeMap;
+  std::map<std::string, std::vector<double> > doubleMap;
+  std::map<std::string, std::vector<int> > intMap;
+  std::map<std::string, std::vector<long> > longMap;
 };
 
 class ImageList : public Table {
-  public:
-    ImageList() : Table("IMAGELIST") {}
+public:
+  ImageList() : Table("IMAGELIST") {}
 };
 
 class Catalogue : public Table {
-  public:
-    Catalogue() : Table("CATALOGUE") {}
-    void render(const SourceFile &s);
+public:
+  Catalogue() : Table("CATALOGUE") {}
+  void render(const SourceFile &s);
 };
 
 #endif /* end of include guard: TABLE_H */

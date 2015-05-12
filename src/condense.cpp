ba3c12f1bc8c4019ee90cb54a91a46f27d5b11f1
Simon Walker <s.r.walker101@googlemail.com>
2015-05-12 14:11:39 +0100

    rendering image hdus

--- src/condense.cpp
+++ src/condense.cpp
@@ -1,22 +1,21 @@
-#include "condense.h"
 #include <iostream>
-#include <memory>
-
-#include <fits++.h>
+#include "condense.h"
+#include "source_file.h"
 
 using namespace std;
 using namespace fitspp;
 
-typedef auto_ptr<FITSFile> pFITSFile;
-
 namespace {
-    pFITSFile openFitsFile(const string &filename) {
-        return auto_ptr<FITSFile>(FITSFile::openFile(filename));
-    }
+auto_ptr<FITSFile> openFitsFile(const string &filename) {
+    return auto_ptr<FITSFile>(FITSFile::openFile(filename));
+}
 
-    pFITSFile createFitsFile(const string &filename) {
-        return auto_ptr<FITSFile>(FITSFile::createFile(filename));
-    }
+auto_ptr<FITSFile> createFitsFile(const string &filename) {
+    return auto_ptr<FITSFile>(FITSFile::createFile(filename));
+}
+
+vector<string> imageHDUNames = {"HJD",  "FLUX", "FLUXERR",
+                                "CCDX", "CCDY", "SKYBKG"};
 }
 
 Condenser::Condenser(const vector<string> &files)
@@ -26,7 +25,7 @@ Condenser::Condenser(const vector<string> &files)
 
 void Condenser::compute_output_file_dimensions() {
     nimages = files.size();
-    pFITSFile first = openFitsFile(this->files[0]);
+    auto_ptr<FITSFile> first = openFitsFile(this->files[0]);
     FITSBinaryTable *data_table = first->findBinaryTable("apm-binarytable");
     napertures = data_table->getNumRows();
 
@@ -38,6 +37,30 @@ void Condenser::render(const string &output_filename) {
     cout << "Rendering to " << output_filename << endl;
     compute_output_file_dimensions();
 
-    pFITSFile output = createFitsFile(output_filename);
+    auto_ptr<FITSFile> output = createFitsFile(output_filename);
+    initialiseOutputFile(output);
+
+    for (int i = 0; i < nimages; i++) {
+        auto name = files[i];
+        auto source_file = SourceFile(name);
+
+        source_file.addMJD(images["HJD"], i, napertures);
+        source_file.addData("Aper_flux_3", images["FLUX"], i, napertures);
+        source_file.addData("Aper_flux_3_err", images["FLUXERR"], i,
+                            napertures);
+        source_file.addData("X_coordinate", images["CCDX"], i, napertures);
+        source_file.addData("Y_coordinate", images["CCDY"], i, napertures);
+        source_file.addData("Sky_level", images["SKYBKG"], i, napertures);
+    }
+
     output->closeFile();
+}
+
+void Condenser::initialiseOutputFile(auto_ptr<FITSFile> &f) {
+    f->addEmptyPrimary();
+    for (auto name : imageHDUNames) {
+        long axes[] = {nimages, napertures};
+        auto image = f->addImage(name, doubleImg, 2, axes);
+        images.insert(pair<string, FITSImage *>(name, image));
+    }
 }

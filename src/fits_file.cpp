90547674b9628d49844bcd192ec33f8a89af883d
Simon Walker <s.r.walker101@googlemail.com>
2015-05-18 09:55:33 +0100

    Add empty initial header
    Former-commit-id: 1ce323403e2b58c9af5e592872e934a5324b2359

--- src/fits_file.cpp
+++ src/fits_file.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
-FITSFile FITSFile::createFile(const string &filename) {
-    FITSFile f;
-    fits_create_file(&f.fptr_, filename.c_str(), &f.status_);
-    f.check();
+FITSFile* FITSFile::createFile(const string &filename) {
+    FITSFile *f = new FITSFile;;
+    fits_create_file(&f->fptr_, filename.c_str(), &f->status_);
+    if (f->status_ == FILE_NOT_CREATED) {
+        remove(filename.c_str());
+        f->status_ = 0;
+        fits_create_file(&f->fptr_, filename.c_str(), &f->status_);
+    }
+    f->check();
+
+    /* Add empty primary */
+    fits_write_imghdr(f->fptr_, 8, 0, NULL, &f->status_);
+    f->check();
     return f;
 }
 

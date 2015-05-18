719c8aa09a9bd1a7d7d8da1ba22b4543eaf33775
Simon Walker <s.r.walker101@googlemail.com>
2015-05-18 19:13:13 +0100

    Extracting most of the data
    Former-commit-id: 76774a91fd47e208da6333460ba50974f41e234e

--- zlp_condense.py
+++ zlp_condense.py
@@ -60,15 +60,24 @@ def main(args):
     print('{:d} images, {:d} apertures'.format(nimages, napertures))
 
     catalogue_data = np.recarray(napertures, dtype=[
-        ('ra', np.float64),
-        ('dec', np.float64),
+        ('OBJ_ID', 'a26'),
+        ('RA', np.float64),
+        ('DEC', np.float64),
+        ('FLUX_MEAN', np.float64),
+        ('FLUX_MEDIAN', np.float64),
+        ('BLEND_FRACTION', np.float32),
         ])
-    catalogue_data['ra'] = np.degrees(first.data['ra'])
-    catalogue_data['dec'] = np.degrees(first.data['dec'])
+    catalogue_data['OBJ_ID'] = np.array(['NG{:06d}'.format(i) for i in
+        np.arange(napertures)])
+    catalogue_data['RA'] = np.degrees(first.data['ra'])
+    catalogue_data['DEC'] = np.degrees(first.data['dec'])
+    catalogue_data['BLEND_FRACTION'] = np.zeros(napertures, dtype=np.float32)
 
     imagelist_data = np.recarray(nimages, dtype=[
-        ('tmid', np.float64),
-        ('airmass', np.float64),
+        ('TMID', np.float64),
+        ('AIRMASS', np.float32),
+        ('IMAGE_ID', np.int64),
+        ('EXPOSURE', np.float32),
         ])
 
     image = lambda name: FITSImage(name, nimages, napertures)
@@ -78,8 +87,10 @@ def main(args):
     for i, filename in enumerate(sorted_images):
         source = SourceFile.open_file(filename)
         mjd = source.header['mjd']
-        imagelist_data['tmid'][i] = mjd
-        imagelist_data['airmass'][i] = source.header['airmass']
+        imagelist_data['TMID'][i] = mjd
+        imagelist_data['AIRMASS'][i] = source.header['airmass']
+        imagelist_data['IMAGE_ID'][i] = source.header['image_id']
+        imagelist_data['EXPOSURE'][i] = source.header['exposure']
 
         image_map['HJD'].set_data(i, mjd + source.data['hjd_correction'])
         image_map['FLUX'].set_data(i, source.data['Aper_flux_3'])
@@ -88,6 +99,8 @@ def main(args):
         image_map['CCDY'].set_data(i, source.data['Y_coordinate'])
         image_map['SKYBKG'].set_data(i, source.data['Sky_level'])
 
+    catalogue_data['FLUX_MEAN'] = np.mean(image_map['FLUX'].data, axis=1)
+    catalogue_data['FLUX_MEDIAN'] = np.median(image_map['FLUX'].data, axis=1)
 
     with create_output_file(args.output) as hdulist:
         hdulist.append(fits.BinTableHDU(imagelist_data, name='IMAGELIST'))
